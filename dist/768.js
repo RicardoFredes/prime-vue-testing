5a1cf8efee91c411d56aa13dc771559c44c934c4
Fredes <ricardofredes@gmail.com>
2024-07-03 10:12:02 -0300

    Add files via upload

---
+++ dist/768.js
@@ -0,0 +1,266 @@
+"use strict";
+(self["webpackChunkprime"] = self["webpackChunkprime"] || []).push([[768],{
+
+/***/ 768:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ TestView)
+});
+
+// EXTERNAL MODULE: ./node_modules/.pnpm/@vue+runtime-core@3.4.21/node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js
+var runtime_core_esm_bundler = __webpack_require__(327);
+// EXTERNAL MODULE: ./node_modules/.pnpm/@vue+reactivity@3.4.21/node_modules/@vue/reactivity/dist/reactivity.esm-bundler.js
+var reactivity_esm_bundler = __webpack_require__(496);
+// EXTERNAL MODULE: ./node_modules/.pnpm/primevue@3.49.1_vue@3.4.21/node_modules/primevue/inputswitch/inputswitch.esm.js + 1 modules
+var inputswitch_esm = __webpack_require__(436);
+// EXTERNAL MODULE: ./node_modules/.pnpm/primevue@3.49.1_vue@3.4.21/node_modules/primevue/button/button.esm.js + 3 modules
+var button_esm = __webpack_require__(472);
+// EXTERNAL MODULE: ./node_modules/.pnpm/primevue@3.49.1_vue@3.4.21/node_modules/primevue/card/card.esm.js + 1 modules
+var card_esm = __webpack_require__(600);
+// EXTERNAL MODULE: ./node_modules/.pnpm/primevue@3.49.1_vue@3.4.21/node_modules/primevue/column/column.esm.js + 1 modules
+var column_esm = __webpack_require__(74);
+// EXTERNAL MODULE: ./node_modules/.pnpm/primevue@3.49.1_vue@3.4.21/node_modules/primevue/datatable/datatable.esm.js + 28 modules
+var datatable_esm = __webpack_require__(823);
+// EXTERNAL MODULE: ./node_modules/.pnpm/primevue@3.49.1_vue@3.4.21/node_modules/primevue/tabview/tabview.esm.js + 2 modules
+var tabview_esm = __webpack_require__(499);
+// EXTERNAL MODULE: ./node_modules/.pnpm/primevue@3.49.1_vue@3.4.21/node_modules/primevue/tabpanel/tabpanel.esm.js + 1 modules
+var tabpanel_esm = __webpack_require__(772);
+// EXTERNAL MODULE: ./node_modules/.pnpm/vue-router@4.3.0_vue@3.4.21/node_modules/vue-router/dist/vue-router.mjs + 5 modules
+var vue_router = __webpack_require__(554);
+;// CONCATENATED MODULE: ./node_modules/.pnpm/vue-loader@16.8.3_@vue+compiler-sfc@3.2.47_vue@3.4.21_webpack@5.89.0_webpack-cli@4.10.0_/node_modules/vue-loader/dist/index.js??ruleSet[1].rules[5].use[0]!./src/views/SaqueView.vue?vue&type=script&setup=true&lang=js
+
+
+const _hoisted_1 = { class: "flex gap-3 items-center my-6" }
+const _hoisted_2 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("span", { class: "material-symbols-rounded" }, " chevron_left ", -1 /* HOISTED */)
+const _hoisted_3 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("h1", { class: "text-xl" }, "Contas bancárias", -1 /* HOISTED */)
+const _hoisted_4 = { class: "grid gap-3" }
+const _hoisted_5 = { class: "flex justify-between" }
+const _hoisted_6 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("h2", { class: "flex gap-1 items-center text-primary-500" }, [
+  /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("span", { class: "material-symbols-rounded text-lg" }, " location_on "),
+  /*#__PURE__*/(0,runtime_core_esm_bundler/* createTextVNode */.Uk)(" Contas nacionais ")
+], -1 /* HOISTED */)
+const _hoisted_7 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("span", { class: "material-symbols-rounded text-base mr-1" }, " add ", -1 /* HOISTED */)
+const _hoisted_8 = { class: "my-6" }
+const _hoisted_9 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("span", { class: "material-symbols-rounded text-base" }, " edit ", -1 /* HOISTED */)
+const _hoisted_10 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("p", { class: "m-0" }, " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ", -1 /* HOISTED */)
+const _hoisted_11 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("p", { class: "m-0" }, " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi. ", -1 /* HOISTED */)
+
+;
+
+
+
+
+
+
+
+
+/* harmony default export */ const SaqueViewvue_type_script_setup_true_lang_js = ({
+  __name: 'SaqueView',
+  setup(__props) {
+
+const companies = [
+  {
+    id: '1',
+    key: '00.000.000/0001-00',
+    type: 'Pix (PJ)',
+    bank: '000 - Nome do banco',
+    companyName: 'Nome da empresa',
+    document: '00.000.000/0001-00',
+  },
+  {
+    id: '2',
+    key: '00.000.000/0001-00',
+    type: 'Pix (PJ)',
+    bank: '000 - Nome do banco',
+    companyName: 'Nome da empresa',
+    document: '00.000.000/0001-00',
+  }
+]
+
+const router = (0,vue_router/* useRouter */.tv)()
+
+const handleClick = () => {
+  router.push('/')
+}
+
+return (_ctx, _cache) => {
+  return ((0,runtime_core_esm_bundler/* openBlock */.wg)(), (0,runtime_core_esm_bundler/* createElementBlock */.iD)(runtime_core_esm_bundler/* Fragment */.HY, null, [
+    (0,runtime_core_esm_bundler/* createElementVNode */._)("header", _hoisted_1, [
+      (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(button_esm/* default */.Z), {
+        "aria-label": "Voltar",
+        rounded: "",
+        size: "small",
+        onClick: handleClick
+      }, {
+        icon: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+          _hoisted_2
+        ]),
+        _: 1 /* STABLE */
+      }),
+      _hoisted_3
+    ]),
+    (0,runtime_core_esm_bundler/* createElementVNode */._)("main", _hoisted_4, [
+      (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(card_esm/* default */.Z), null, {
+        content: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+          (0,runtime_core_esm_bundler/* createElementVNode */._)("header", _hoisted_5, [
+            _hoisted_6,
+            (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(button_esm/* default */.Z), {
+              size: "small",
+              label: "Cadastrar nova conta"
+            }, {
+              icon: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+                _hoisted_7
+              ]),
+              _: 1 /* STABLE */
+            })
+          ]),
+          (0,runtime_core_esm_bundler/* createElementVNode */._)("div", _hoisted_8, [
+            (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(datatable_esm/* default */.Z), {
+              value: companies,
+              tableStyle: "min-width: 50rem"
+            }, {
+              default: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+                (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(column_esm/* default */.Z), {
+                  field: "key",
+                  header: "Chave"
+                }),
+                (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(column_esm/* default */.Z), {
+                  field: "type",
+                  header: "Tipo"
+                }),
+                (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(column_esm/* default */.Z), {
+                  field: "bank",
+                  header: "Banco"
+                }),
+                (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(column_esm/* default */.Z), {
+                  field: "companyName",
+                  header: "Empresa"
+                }),
+                (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(column_esm/* default */.Z), {
+                  field: "document",
+                  header: "CNPJ/CPF"
+                }),
+                (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(column_esm/* default */.Z), { header: "" }, {
+                  body: (0,runtime_core_esm_bundler/* withCtx */.w5)((slotProps) => [
+                    (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(button_esm/* default */.Z), {
+                      onClick: $event => (console.log(`/dados-bancarios/${slotProps.data.id}`)),
+                      text: "",
+                      "aria-label": "Editar",
+                      rounded: ""
+                    }, {
+                      icon: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+                        _hoisted_9
+                      ]),
+                      _: 2 /* DYNAMIC */
+                    }, 1032 /* PROPS, DYNAMIC_SLOTS */, ["onClick"])
+                  ]),
+                  _: 1 /* STABLE */
+                })
+              ]),
+              _: 1 /* STABLE */
+            })
+          ])
+        ]),
+        _: 1 /* STABLE */
+      }),
+      (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(card_esm/* default */.Z), null, {
+        content: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+          (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(tabview_esm/* default */.Z), null, {
+            default: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+              (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(tabpanel_esm/* default */.Z), { header: "Recebimentos futuros" }, {
+                default: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+                  _hoisted_10
+                ]),
+                _: 1 /* STABLE */
+              }),
+              (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(tabpanel_esm/* default */.Z), { header: "Histórico de solicitações" }, {
+                default: (0,runtime_core_esm_bundler/* withCtx */.w5)(() => [
+                  _hoisted_11
+                ]),
+                _: 1 /* STABLE */
+              })
+            ]),
+            _: 1 /* STABLE */
+          })
+        ]),
+        _: 1 /* STABLE */
+      })
+    ])
+  ], 64 /* STABLE_FRAGMENT */))
+}
+}
+
+});
+;// CONCATENATED MODULE: ./src/views/SaqueView.vue?vue&type=script&setup=true&lang=js
+ 
+;// CONCATENATED MODULE: ./src/views/SaqueView.vue
+
+
+
+const __exports__ = SaqueViewvue_type_script_setup_true_lang_js;
+
+/* harmony default export */ const SaqueView = (__exports__);
+;// CONCATENATED MODULE: ./node_modules/.pnpm/vue-loader@16.8.3_@vue+compiler-sfc@3.2.47_vue@3.4.21_webpack@5.89.0_webpack-cli@4.10.0_/node_modules/vue-loader/dist/index.js??ruleSet[1].rules[5].use[0]!./src/views/TestView.vue?vue&type=script&setup=true&lang=js
+
+
+
+const TestViewvue_type_script_setup_true_lang_js_hoisted_1 = { class: "max-w-screen-lg mx-auto py-5" }
+const TestViewvue_type_script_setup_true_lang_js_hoisted_2 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("label", { class: "cursor-pointer" }, "Light/Dark Mode", -1 /* HOISTED */)
+const TestViewvue_type_script_setup_true_lang_js_hoisted_3 = /*#__PURE__*/(0,runtime_core_esm_bundler/* createElementVNode */._)("hr", { class: "my-5" }, null, -1 /* HOISTED */)
+
+;
+
+
+
+
+/* harmony default export */ const TestViewvue_type_script_setup_true_lang_js = ({
+  __name: 'TestView',
+  props: {
+    "modelValue": {},
+    "modelModifiers": {},
+  },
+  emits: ["update:modelValue"],
+  setup(__props) {
+
+const checked = (0,runtime_core_esm_bundler/* useModel */.tT)(__props, "modelValue")
+
+const toggleDarkMode = () => {
+  checked.value = !checked.value
+  window.document.documentElement.classList.toggle('dark')
+}
+
+return (_ctx, _cache) => {
+  return ((0,runtime_core_esm_bundler/* openBlock */.wg)(), (0,runtime_core_esm_bundler/* createElementBlock */.iD)("main", TestViewvue_type_script_setup_true_lang_js_hoisted_1, [
+    (0,runtime_core_esm_bundler/* createElementVNode */._)("div", {
+      class: "flex items-center gap-3",
+      onClick: toggleDarkMode
+    }, [
+      (0,runtime_core_esm_bundler/* createVNode */.Wm)((0,reactivity_esm_bundler/* unref */.SU)(inputswitch_esm/* default */.Z), { "model-value": checked.value }, null, 8 /* PROPS */, ["model-value"]),
+      TestViewvue_type_script_setup_true_lang_js_hoisted_2
+    ]),
+    TestViewvue_type_script_setup_true_lang_js_hoisted_3,
+    (0,runtime_core_esm_bundler/* createVNode */.Wm)(SaqueView)
+  ]))
+}
+}
+
+});
+;// CONCATENATED MODULE: ./src/views/TestView.vue?vue&type=script&setup=true&lang=js
+ 
+;// CONCATENATED MODULE: ./src/views/TestView.vue
+
+
+
+const TestView_exports_ = TestViewvue_type_script_setup_true_lang_js;
+
+/* harmony default export */ const TestView = (TestView_exports_);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=768.js.map
